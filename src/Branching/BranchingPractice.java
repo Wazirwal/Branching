383667f22c8ab9919e6bb7be98e69f612f2cb746
gitpackage <noor@gmail.com>
2022-11-09 20:15:20 -0500

    New Changes

--- src/Branching/BranchingPractice.java
+++ src/Branching/BranchingPractice.java
@@ -3,8 +3,11 @@ package Branching;
 public class BranchingPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	public void test() {
+		System.out.println("This method is on review Session Branch");
 	}
 
 }
